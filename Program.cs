27b6abeeaebb01ec143f409fc963756cb36d2bff
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 23:16:48 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap vao so luong so nguyen to can in ra:");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+
+        int count = 0;
+
+        int N = 2;
+
+        while (count < numbers)
+        {
+            if (IsPrime(N))
+            {
+                Console.WriteLine(N);
+                count++;
+            }
+            N++;
+        }
+    }
+    static bool IsPrime(int n)
+    {
+        if (n <= 1) return false; 
+        for (int i = 2; i <= Math.Sqrt(n); i++) 
+        {
+            if (n % i == 0) return false; 
+        }
+        return true; 
+    }
+}
